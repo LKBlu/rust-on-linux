d178a30fa7805344c9046e8bcc47002d008a682f
Filipe Xavier <43504172+felipeagger@users.noreply.github.com>
2024-11-02 09:37:09 -0300

    Update rust_lock_module.rs

--- rust-module-sample/rust_lock_module.rs
+++ rust-module-sample/rust_lock_module.rs
@@ -1,99 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0
+
 //! Rust out-of-tree sample
 
 use kernel::prelude::*;
-use kernel::sync::{new_mutex, new_spinlock, Mutex, SpinLock};
 
 module! {
-    type: RustLockModule,
-    name: "rust_lock_module",
+    type: RustOutOfTree,
+    name: "rust_out_of_tree",
     author: "Rust for Linux Contributors",
-    description: "Rust locks sample",
+    description: "Rust out-of-tree sample",
     license: "GPL",
 }
 
-struct Inner {
-    a: u32,
-    b: u32,
+struct RustOutOfTree {
+    numbers: KVec<i32>,
 }
 
-#[pin_data]
-struct ExampleMtx {
-    #[pin]
-    d: Mutex<Inner>,
-}
-
-#[pin_data]
-struct ExampleSpin {
-    #[pin]
-    d: SpinLock<Inner>,
-}
-
-impl ExampleMtx {
-    fn new() -> impl PinInit<Self> {
-        let (a, b) = (5, 10);
-        pr_info!("Mutex Init Values - a: {}, b: {}\n", a, b);
-        
-        pin_init!(Self {
-            d <- new_mutex!(Inner { a: a, b: b }),
-        })
-    }
-}
-
-impl ExampleSpin {
-    fn new() -> impl PinInit<Self> {
-        let (a, b) = (3, 6);
-        pr_info!("Spin Init Values - a: {}, b: {}\n", a, b);
-        
-        pin_init!(Self {
-            d <- new_spinlock!(Inner { a: a, b: b }),
-        })
-    }
-}
-
-struct RustLockModule {
-    example_mtx: Pin<KBox<ExampleMtx>>,
-    example_spin: Pin<KBox<ExampleSpin>>,
-}
-
-impl kernel::Module for RustLockModule {
+impl kernel::Module for RustOutOfTree {
     fn init(_module: &'static ThisModule) -> Result<Self> {
-        pr_info!("Rust module lock sample (init)\n");
+        pr_info!("Rust out-of-tree sample (init)\n");
 
-        let example_mtx = KBox::pin_init(ExampleMtx::new(), GFP_KERNEL)?;
-        let example_spin = KBox::pin_init(ExampleSpin::new(), GFP_KERNEL)?;
+        let mut numbers = KVec::new();
+        numbers.push(72, GFP_KERNEL)?;
+        numbers.push(108, GFP_KERNEL)?;
+        numbers.push(200, GFP_KERNEL)?;
 
-        Ok(RustLockModule {example_mtx, example_spin})
+        Ok(RustOutOfTree { numbers })
     }
 }
 
-impl RustLockModule {
-
-    fn example_mtx_operation(m: &Pin<KBox<ExampleMtx>>) {
-        let mut guard = m.d.lock();
-        guard.a += 10;
-        guard.b += 20;
-    }
-
-    fn example_spin_operation(m: &Pin<KBox<ExampleSpin>>) {
-        let mut guard = m.d.lock();
-        guard.a += 9;
-        guard.b += 14;
-    }
-}
-
-impl Drop for RustLockModule {
+impl Drop for RustOutOfTree {
     fn drop(&mut self) {
-        Self::example_mtx_operation(&self.example_mtx);
-        Self::example_spin_operation(&self.example_spin);
-
-        if let Some(guard) = self.example_mtx.d.try_lock() {
-            pr_info!("Mutex Values - a: {}, b: {}\n", guard.a, guard.b);
-        }
-
-        if let Some(guard) = self.example_spin.d.try_lock() {
-            pr_info!("Spin Values - a: {}, b: {}\n", guard.a, guard.b);
-        }
-
-        pr_info!("Rust lock sample (exit)\n");
+        pr_info!("My numbers are {:?}\n", self.numbers);
+        pr_info!("Rust out-of-tree sample (exit)\n");
     }
 }
